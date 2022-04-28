3d4b5baa56949d2115786b676655390c28290d79
JosephhBh <j.bhanini6@gmail.com>
2022-04-28 15:10:41 +0300

    adding about us UI

--- lib/core/view/about_us_screen.dart
+++ lib/core/view/about_us_screen.dart
@@ -1,8 +1,64 @@
+// import 'package:flutter/material.dart';
+// import 'package:tipperapp/core/device_utils/device_utils.dart';
+// import 'package:tipperapp/core/navigation/navigation_service.dart';
+// import 'package:tipperapp/locator.dart';
+// import 'package:tipperapp/widgets/icons/back_icon.dart';
+// import 'package:tipperapp/widgets/scaffold/global_scaffold.dart';
+// import 'package:tipperapp/widgets/text/global_text.dart';
+
+// class AboutUsPage extends StatelessWidget {
+//   final NavigationService _navigationService = locator<NavigationService>();
+//   @override
+//   Widget build(BuildContext context) {
+//     return GlobalScaffold(
+//       backgroundColor: appColor.greyColor,
+//       child: Stack(
+//         fit: StackFit.loose,
+//         children: [
+//           Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               heighSpacer(18),
+//               GestureDetector(
+//                 onTap: () {
+//                   _navigationService.pop();
+//                 },
+//                 child: Container(
+//                   color: appColor.transparentColor,
+//                   child: Row(
+//                     children: [
+//                       widthSpacer(17),
+//                       BackIcon(),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//           Align(
+//             alignment: Alignment.center,
+//             child: Container(
+//               width: setCurrentWidth(300),
+//               child: GlobalText(
+//                 text:
+//                     "Tipr is a mobile application that allows customers to pay cashless tips directly to service workers using QR Code reader technology.",
+//                 showEllipsis: false,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tipperapp/core/device_utils/device_utils.dart';
 import 'package:tipperapp/core/navigation/navigation_service.dart';
 import 'package:tipperapp/locator.dart';
 import 'package:tipperapp/widgets/icons/back_icon.dart';
+import 'package:tipperapp/widgets/icons/back_icon_withcolor.dart';
 import 'package:tipperapp/widgets/scaffold/global_scaffold.dart';
 import 'package:tipperapp/widgets/text/global_text.dart';
 
@@ -15,35 +71,104 @@ class AboutUsPage extends StatelessWidget {
       child: Stack(
         fit: StackFit.loose,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              heighSpacer(18),
-              GestureDetector(
-                onTap: () {
-                  _navigationService.pop();
-                },
-                child: Container(
-                  color: appColor.transparentColor,
-                  child: Row(
-                    children: [
-                      widthSpacer(17),
-                      BackIcon(),
-                    ],
-                  ),
-                ),
+          Container(
+            height: setCurrentHeight(355),
+            width: double.infinity,
+            color: appColor.yellowColor,
+            child: Center(
+              child: GlobalText(
+                color: appColor.whiteColor,
+                text: "tipr",
+                fontSize: 80,
               ),
-            ],
+            ),
           ),
           Align(
-            alignment: Alignment.center,
-            child: Container(
-              width: setCurrentWidth(300),
-              child: GlobalText(
-                text:
-                    "Tipr is a mobile application that allows customers to pay cashless tips directly to service workers using QR Code reader technology.",
-                showEllipsis: false,
-              ),
+            alignment: Alignment.topLeft,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                heighSpacer(18),
+                GestureDetector(
+                  onTap: () {
+                    _navigationService.pop();
+                  },
+                  child: Container(
+                    color: appColor.transparentColor,
+                    child: Row(
+                      children: [
+                        widthSpacer(17),
+                        BackIconWithColor(
+                          color: appColor.whiteColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Align(
+          //   alignment: Alignment.center,
+          //   child: Container(
+          //     width: setCurrentWidth(300),
+          //     child: GlobalText(
+          //       text:
+          //           "Tipr is a mobile application that allows customers to pay cashless tips directly to service workers using QR Code reader technology.",
+          //       showEllipsis: false,
+          //     ),
+          //   ),
+          // ),
+          applyPadding(
+            390,
+            0,
+            24,
+            0,
+            Row(
+              // textBaseline: TextBaseline.ideographic,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                applyPadding(
+                  20,
+                  0,
+                  0,
+                  0,
+                  Container(
+                    width: setCurrentWidth(70),
+                    child: Divider(
+                      height: 2,
+                      thickness: 3,
+                      color: appColor.yellowColor,
+                    ),
+                  ),
+                ),
+                widthSpacer(5),
+                Expanded(
+                  flex: 1,
+                  child: RichText(
+                    text: TextSpan(
+                      text: '',
+                      // style: DefaultTextStyle.of(context).style,
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Tipr',
+                          style: GoogleFonts.fredokaOne(
+                              fontSize: setFontSize(30),
+                              color: appColor.yellowColor.withOpacity(0.6)),
+                        ),
+                        TextSpan(
+                          text:
+                              ' is a mobile application that allows customers to pay cashless tips directly to service workers using QR Code reader technology.',
+                          style: GoogleFonts.fredokaOne(
+                              fontSize: setFontSize(30),
+                              color: appColor.blackColor.withOpacity(0.6)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
